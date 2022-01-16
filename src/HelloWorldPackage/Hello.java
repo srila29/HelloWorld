6990f49165017be7df6184b6eae1170418eeeac2
srila29 <srilakshmicompsci2021@gmail.com>
2022-01-16 12:39:51 +0400

    Changes updated - Integer

--- src/HelloWorldPackage/Hello.java
+++ src/HelloWorldPackage/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.print("Hello");
+      System.out.print("Hello\n");
+      System.out.println(5+5+2);
 	}
 
 }
